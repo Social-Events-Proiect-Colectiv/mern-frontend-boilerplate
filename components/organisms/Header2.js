255ed3adf8b7b02a8122f69cf2581b0fdafd55f3
Andreea <130660031+andreeaisac0807@users.noreply.github.com>
2023-05-11 13:50:34 +0300

    Header and other little changes

--- components/organisms/Header2.js
+++ components/organisms/Header2.js
@@ -5,15 +5,15 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <><>
+    <>
       <Navbar bg="blue" expand="lg">
         <Navbar.Toggle aria-controls="navbarExample01" />
         <Navbar.Collapse id="navbarExample01">
           <Nav className="me-auto">
-            <Link href="/home">
+            <Link href="#">
               <Nav.Link>Home</Nav.Link>
             </Link>
-            <Link href="/about">
+            <Link href="#">
               <Nav.Link>About Us</Nav.Link>
             </Link>
           </Nav>
@@ -22,12 +22,7 @@ const Header = () => {
       <div className={styles.title}>
         <h1>Social Company Events</h1>
       </div>
-    </><div className={styles.container}>
-        <h2 className={styles.logo}>Logo</h2>
-        <h1>Company Social Events</h1>
-        <Button label="ABOUT US" className="button" />
-
-      </div></>
+    </>
   );
 };
 
--- pages/AboutUsPage.js
+++ pages/AboutUsPage.js
@@ -8,9 +8,10 @@ export default function HomePage() {
         className='w-100 d-block'
         itemId={1}
         src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg'
+       //src='images\virtual-planning-companies.png'
         alt='...'>
-        <h5>Welcome to our website!</h5>
-        <p>This site was created with the aim of providing a better organization of events within companies</p>
+        <h5>Welcome to our application!</h5>
+        <p>This app was created with the aim of providing a better organization of events within companies</p>
       </MDBCarouselItem>
       <MDBCarouselItem
         className='w-100 d-block'
@@ -27,9 +28,8 @@ export default function HomePage() {
         src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg'
         alt='...'>
         <h5>Contact</h5>
-        <p>071234567890</p>
-        <p>or</p>
-        <p>CompanyEvents@yahoo.com</p>
+        <p>Team members: Feil Nadia and Isac Andreea</p>
+        <p>nadia.feil02@e-uvt.ro or andreea.isac02@e-uvt.ro</p>
 
       </MDBCarouselItem>
     </MDBCarousel>
