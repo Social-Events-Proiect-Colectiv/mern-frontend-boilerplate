1ecbcc3d931654f7e329c5ba85f4f0f98b67c40d
zeulaprograme <danthecol@gmail.com>
2023-05-14 00:04:31 +0300

    changes to project setup and register

--- pages/_app.js
+++ pages/_app.js
@@ -5,3 +5,4 @@ import "@fortawesome/fontawesome-free/css/all.min.css"
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Link from 'next/link';
 import Footer from '@/components/organisms/Footer';
 import Header from '@/components/organisms/Header2';
 
-function Register() {
+export default class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            fname:"",
+            lname:"",
+            email:"",
+            password:"",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      const { fname, lname, email, password } = this.state;
+      console.log(fname, lname, email, password);
+      fetch("http://localhost:5000/register", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          fname,
+          lname,
+          email,
+          password,
+        }),
+      })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data); // Log the response data
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+    
+    render(){
   return (
     <div>
       <Header />
@@ -12,14 +56,33 @@ function Register() {
         <div className="row justify-content-center">
           <div className="col-md-6">
             <h2>Register</h2>
-            <form>
-              <div className="mb-4">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control form-control-lg" id="email" />
+            <form onSubmit={this.handleSubmit}>
+            <div className="mb-4">
+                <label htmlFor="fname" className="form-label">First Name</label>
+                <input type="text" className="form-control form-control-lg" id="fname" 
+                                onChange={(e) => this.setState({fname:e.target.value})}
+
+                />
               </div>
               <div className="mb-4">
+                <label htmlFor="lname" className="form-label">Last Name</label>
+                <input type="text" className="form-control form-control-lg" id="lname" 
+                                onChange={(e) => this.setState({lname:e.target.value})}
+
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input 
+                type="text" className="form-control form-control-lg" id="email"
+                onChange={(e) => this.setState({email:e.target.value})}
+                />
+              </div>  
+              <div className="mb-4">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control form-control-lg" id="password" />
+                <input type="password" className="form-control form-control-lg" id="password"
+                                onChange={(e) => this.setState({password:e.target.value})}
+                />
               </div>
               <div className="text-center text-md-start mt-4 pt-2">
                 <button className="btn btn-primary px-5" type="submit">Register</button>
@@ -34,5 +97,4 @@ function Register() {
     </div>
   );
 }
-
-export default Register;
+}
